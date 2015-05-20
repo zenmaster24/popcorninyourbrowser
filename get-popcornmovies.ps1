3e8995582b461e2bef4cf80cab3bdf41240f74b1
zenmaster24 <zenmaster24@hotmail.com>
2015-05-20 21:30:49 +1000

    Initial commit

---
+++ get-popcornmovies.ps1
@@ -0,0 +1,23 @@
+﻿Param (
+    [Parameter(Mandatory=$True)]
+		[string[]]$orderby
+)
+
+$Movies = (Invoke-WebRequest http://popcorninyourbrowser.net/js/movies.js).Content
+$HeaderPattern = '(?i){(.*?)}'
+
+$MovieArray= @()
+
+$Movies = ([regex]$HeaderPattern).Matches($Movies)|ConvertFrom-Json
+
+Foreach ($Movie in $Movies){
+    $MovieList = New-Object -TypeName PSObject
+    $MovieList | Add-Member -Type NoteProperty -Name Title -value $Movie.Title
+    $MovieList | Add-Member -Type NoteProperty -Name Year -value $Movie.Year
+    $MovieList | Add-Member -Type NoteProperty -Name Cover -value $Movie.Cover
+    
+    $MovieArray += $MovieList
+}
+
+$MovieArray | Sort -Property $OrderBy | ft -AutoSize
+"$($moviearray.count) movies found."
